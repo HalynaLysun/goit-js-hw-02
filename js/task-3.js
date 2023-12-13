5aefee802ab61066aeb38d881fd1d703bb02fcde
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2023-12-13 06:23:17 +0200

    Update task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,22 @@
 "use strict"
+
+function checkForSpam(message) {
+    const lowercaseInput = message.toLowerCase()
+    const uppercaseInput = message.toUpperCase()
+    if (lowercaseInput.includes("spam") ||
+        uppercaseInput.includes("spam") ||
+        lowercaseInput.includes("sale") ||
+        uppercaseInput.includes("sale")) {
+            return true
+    } else {
+        return false
+    }
+}
+
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter")); 
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!")); 
+console.log(checkForSpam("Trust me, this is not a spam message")); 
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); 
+console.log(checkForSpam("[SPAM] How to earn fast money?")); 
